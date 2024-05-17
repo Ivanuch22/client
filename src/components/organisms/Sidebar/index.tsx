3b23bac14a4fdc44dbbc895a3dde80e14f658c94
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-05-17 14:11:27 +0300

    dfa;lkjs

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -13,17 +13,19 @@ interface SidebarProps {
 
 export default function Sidebar({ randomBanner, children }: SidebarProps) {
   return (
-    <div className=' col-md-auto  mx-360'>
+    <div className=' col-md-auto  mx-360' >
     <article className=" col-md-auto  pe-xl-3 col-sm-12 d-flex flex-wrap flex-column flex-sm-row align-items-center align-items-sm-start justify-content-sm-start justify-content-md-start flex-md-column col-md-auto">
-      {/* displaying randomBanner */}
       <div className="sidebar-section sidebar-section--banner pe-xl-3">
-        <a href={randomBanner?.url} target="_blank">
+        <a href={randomBanner?.url} 
+            style={{width: "100%", display: "block"}}
+            target="_blank">
           <img
             className="sidebar-banner"
             src={randomBanner?.image}
             width={300}
             height={250}
             alt={randomBanner?.alt}
+            style={{width: "100%"}}
           />
         </a>
       </div>
