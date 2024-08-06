b5a8e050bb53530d07e24285a468081d9039e489
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-08-06 11:29:02 +0300

    remove body from most popular

--- src/utils/getRandomPopularNews.ts
+++ src/utils/getRandomPopularNews.ts
@@ -1,30 +1,37 @@
 import { server } from '@/http';
 
-const fieldsToPopulate = [  "seo_title",
-"page_title",
-"seo_description",
-"url",
-"keywords",
-"faq",
-"extraLinks",
-"code",
-"rating",
-"article",
-"howto",
-"image",
-"admin_date",
-"heading",
-"is_popular",
-"views",
-"comments",]; 
+// const populateParams = fieldsToPopulate.map(field => `populate=${field}`).join('&');
 
-const populateParams = fieldsToPopulate.map(field => `populate=${field}`).join('&');
+// console.log(populateParams,"some params")
 
-console.log(populateParams,"some params")
+
+const fieldsToPopulate = [
+  "seo_title",
+  "page_title",
+  "seo_description",
+  "url",
+  "keywords",
+  "faq",
+  "extraLinks",
+  "code",
+  "rating",
+  "article",
+  "howto",
+  "image",
+  "admin_date",
+  "heading",
+  "is_popular",
+  "views",
+  "comments",
+];
+
+const fieldsQuery = fieldsToPopulate.filter(field => !["image", "comments"].includes(field)).map(field => `fields=${field}`).join('&');
+const populateQuery = "populate=image&populate=comments";
 
 
 export default async function getRandomPopularNews(locale: string) {
-  const getPage = await server.get(`/blogs?populate=image&populate=comments&locale=${locale === 'ua' ? 'uk' : locale}&pagination[page]=1&pagination[pageSize]=${50}&filters[is_popular][$eq]=true`);
+  // const getPage = await server.get(`/blogs?populate=image&populate=comments&locale=${locale === 'ua' ? 'uk' : locale}&pagination[page]=1&pagination[pageSize]=${50}&filters[is_popular][$eq]=true`);
+  const getPage = await server.get(`/blogs?${fieldsQuery}&${populateQuery}&locale=${locale === 'ua' ? 'uk' : locale}&pagination[page]=1&pagination[pageSize]=50&filters[is_popular][$eq]=true`);
 
   function getRandomSample(arr:any[]) {
     const shuffled = arr.sort(() => Math.random() - 0.5);
