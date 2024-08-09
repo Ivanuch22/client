b4f17b3207f926ff8dc7aa4811abe1a0f219ac28
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-08-09 11:26:23 +0300

    testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,8 +54,8 @@ export default function Home({
       <Head>
         <title>{title}</title>
         <div dangerouslySetInnerHTML={{ __html: hreflangTags }} />
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
+        {/* <meta name="description" content={description} /> */}
+        {/* <meta name="keywords" content={keywords} /> */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
