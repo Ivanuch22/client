c01c18658fabd618d4cae67e1c441c04f9b22f4c
Ivanuch22 <ivan.kalunuch12@gmail.com>
2024-09-27 17:53:40 +0300

    change formatDateTime

--- src/utils/formateDateTime.ts
+++ src/utils/formateDateTime.ts
@@ -1,13 +1,13 @@
-export default function formatDateTime(dateTime:string, isShowHours=false) {
-    const date = new Date(dateTime);
+export default function formatDateTime(dateTime: string, isShowHours = false) {
+  const date = new Date(dateTime);
+  date.setUTCHours(date.getUTCHours() + 3);
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const year = date.getUTCFullYear();
 
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const year = date.getUTCFullYear();
+  const hours = String(date.getUTCHours()).padStart(2, '0');
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
 
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-
-    return isShowHours?`${day}.${month}.${year} ${hours}:${minutes}`:`${day}.${month}.${year}` ;
-  }
+  return isShowHours ? `${day}.${month}.${year} ${hours}:${minutes}` : `${day}.${month}.${year}`;
+}
 
